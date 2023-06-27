f41ddc6c415c517aef38ab6fbd502f47a1e29280
Student-99 <denis.0131@mail.ru>
2023-06-27 19:05:12 +0500

    Еще одно изсправление

--- HomeWork3/src/main/java/ru/yandex/Calculate.java
+++ HomeWork3/src/main/java/ru/yandex/Calculate.java
@@ -8,7 +8,7 @@ public class Calculate {
             do {
                 sum +=number%10;
                 number = number/10;
-            }while ((number%10>0)|(number/10>0));
+            }while (number>0);
             number = (long) sum;
         }while (number/10>0);
         return number;
