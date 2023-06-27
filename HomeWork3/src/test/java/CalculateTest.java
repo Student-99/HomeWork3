f838ee8ab2ba6766d1068fc11221441799fb21c5
Student-99 <denis.0131@mail.ru>
2023-06-27 18:59:04 +0500

    Исправил багу

--- HomeWork3/src/test/java/CalculateTest.java
+++ HomeWork3/src/test/java/CalculateTest.java
@@ -10,11 +10,14 @@ public class CalculateTest {
     @ParameterizedTest
     @CsvSource({
         "1,1",
-        "12,3",
-        "9999999,9",
         "0,0",
+        "10,1",
+        "12,3",
+        "101,2",
         "123,6",
-        "1111111,7"
+        "1010,2",
+        "1111111,7",
+        "9999999,9"
     })
     public void CalculateTest(Long number, Long expected ){
         Assertions.assertEquals(Calculate.calculate(number),expected);
