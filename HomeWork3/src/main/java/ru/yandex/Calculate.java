dd814841569a1810fbf1ffa1489eec3c90c86327
Student-99 <denis.0131@mail.ru>
2023-06-27 19:12:41 +0500

    Еще одно улучшение

--- HomeWork3/src/main/java/ru/yandex/Calculate.java
+++ HomeWork3/src/main/java/ru/yandex/Calculate.java
@@ -1,16 +1,16 @@
 package ru.yandex;
 
 public class Calculate {
-    public static Long calculate(Long number){
+    public static Long calculate(Long number) {
 
-        do{
+        while (number > 9) {
             int sum = 0;
             do {
-                sum +=number%10;
-                number = number/10;
-            }while (number>0);
+                sum += number % 10;
+                number = number / 10;
+            } while (number > 0);
             number = (long) sum;
-        }while (number/10>0);
+        }
         return number;
     }
 }
